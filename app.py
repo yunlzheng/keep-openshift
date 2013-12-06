2187e337d78eccfc738603424386b9ab9afde088
yunlzheng <yunlzheng@gmail.com>
2013-12-06 08:25:30 +0000

    update app.py

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
+# coding: utf-8
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello():
-    return "Hello World!"
-    
-if __name__ == '__mian__':
+    return 'Hello World!'
+
+if __name__ == '__main__':
     app.debug = True
-    app.run('0.0.0.0')
-        
-    
-
-
+    app.run(host='0.0.0.0')
