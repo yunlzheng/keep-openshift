bd62137cf0903bf5b310a03feb0b5551d571f812
yunlzheng <yunlzheng@gmail.com>
2013-12-06 06:30:16 +0000

    just try

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,6 +2,14 @@
 
 angular.module('bookroadOpenshiftApp')
   .controller('MainCtrl', function ($scope) {
+    
+    var books = [
+       { name: '代码简洁之道'},
+       { name: '重构改善既有代码的质� '}
+    ];
+
+    $scope.books = books;
+
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
